9206cb783c982e1232172b195ad0b6a3963b705b
Kasprovskyy11 <lokalnytrener@gmail.com>
2025-12-30 17:32:01 +0100

    Register site v2

--- frontend/src/components/forms/CustomButton.tsx
+++ frontend/src/components/forms/CustomButton.tsx
@@ -1,10 +1,17 @@
 interface CustomButtonProps {
   text: string;
+  onClickFunction?: () => void;
 }
 
-export default function CustomButton({ text }: CustomButtonProps) {
+export default function CustomButton({
+  text,
+  onClickFunction,
+}: CustomButtonProps) {
   return (
-    <div className="bg-white text-black text-xl font-bold w-full h-12 flex justify-center items-center rounded-md hover:bg-gray-200 cursor-pointer">
+    <div
+      className="bg-white text-black text-xl font-bold w-full h-12 flex justify-center items-center rounded-md hover:bg-gray-200 cursor-pointer"
+      onClick={onClickFunction}
+    >
       <p>{text}</p>
     </div>
   );
--- frontend/src/components/forms/CustomInput.tsx
+++ frontend/src/components/forms/CustomInput.tsx
@@ -8,6 +8,7 @@ export default function CustomInput({ type, placeholder }: CustomInputProps) {
     <input
       type={type}
       placeholder={placeholder}
+      maxLength={32}
       className="border-2 border-[#868686] outline-none text-white text-center w-full h-12 rounded-md"
     ></input>
   );
