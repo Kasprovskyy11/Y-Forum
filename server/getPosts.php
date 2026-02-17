3962e48b67c7d032ade1d244404a7a9b97db2c31
Piotr <piotrjar13@gmail.com>
2026-02-17 11:00:08 +0100

    id posta

--- server/getPosts.php
+++ server/getPosts.php
@@ -16,6 +16,7 @@ try {
 
 $stmt = $pdo->query("
     SELECT 
+        p.id_p AS id,
         u.name AS name,
         u.username AS username,
         u.profilowe AS profilePhoto,
@@ -34,11 +35,12 @@ $posts = [];
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
     $posts[] = [
-        "name" => $row["name"],                      // login
-        "username" => $row["username"],              // wyświetlana nazwa
+        "id" => (int)$row["id"],                    // <-- ID POSTA
+        "name" => $row["name"],
+        "username" => $row["username"],
         "date" => $row["date"],
         "text" => $row["text"],
-        "profilePhoto" => $row["profilePhoto"],      // zdjęcie profilowe
+        "profilePhoto" => $row["profilePhoto"],
         "photo" => $row["photo"],
         "likes" => (int)$row["likes"]
     ];
