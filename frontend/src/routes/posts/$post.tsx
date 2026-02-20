f314ee373813f1d4c15a2478fe4d580c8df165bd
Kasprovskyy <lokalnytrener@gmail.com>
2026-02-20 15:59:11 +0100

    Post view fixed

--- frontend/src/routes/posts/$post.tsx
+++ frontend/src/routes/posts/$post.tsx
@@ -2,6 +2,8 @@ import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 import { getPostData } from "../../services/getPostDataService";
 import type { PostData } from "../../services/getPostDataService";
+import LeftPanel from "../../components/UI/DesktopUI/LeftPanel";
+import LowerPanel from "../../components/UI/MobileUIComponents/LowerPanel";
 
 export const Route = createFileRoute("/posts/$post")({
   component: PostPage,
@@ -10,6 +12,7 @@ export const Route = createFileRoute("/posts/$post")({
 function PostPage() {
   const { post } = Route.useParams();
   const [data, setData] = useState<PostData>();
+
   useEffect(() => {
     const fetchPostData = async () => {
       try {
@@ -24,5 +27,30 @@ function PostPage() {
 
     fetchPostData();
   }, [post]);
-  return <div>{data?.text}</div>;
+
+  return (
+    <div className="flex flex-col h-screen items-center lg:grid lg:grid-cols-[1fr_4fr_0fr] ">
+      <LeftPanel />
+
+      {/* Middle column - scrollable */}
+      <div className="h-screen overflow-y-auto text-white">
+        <div className="m-6 border border-[#2F3336] min-h-[80vh]">
+          <div className="border-b border-[#2F3336] flex flex-wrap items-center gap-4 p-4 lg:gap-20 lg:pl-10">
+            <div className="w-16 h-16 rounded-full bg-white flex-shrink-0"></div>
+            <p className="font-medium">{data?.username}</p>
+            <p className="opacity-60">{data?.name}</p>
+            <p className="opacity-60">{data?.date}</p>
+          </div>
+          <div className="p-4 lg:p-12">
+            <p className="whitespace-pre-wrap">{data?.text}</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Right panel - fixed */}
+      <div className="h-screen overflow-y-auto">
+        <LowerPanel />
+      </div>
+    </div>
+  );
 }
